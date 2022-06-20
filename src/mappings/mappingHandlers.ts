ba7aef2cbdc30d126c45cace040712f6d3b7b84d
Erin Shaben <eshaben@icloud.com>
2022-06-19 23:57:47 -0400

    add subquery project for asset transfers

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,19 @@
+import { Transfer } from "../types";
+import { SubstrateEvent } from '@subql/types';
+import { Balance } from "@polkadot/types/interfaces";
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+    const assetId = event.event.data[0];
+    const from = event.event.data[1];
+    const to = event.event.data[2];
+    const balance = event.event.data[0];
+
+    const transfer = new Transfer(`${event.block.block.header.number.toNumber()}-${event.idx}`);
+
+    transfer.assetId = assetId.toString();
+    transfer.from = from.toString();
+    transfer.to = to.toString()
+    transfer.balance = (balance as Balance).toBigInt();
+
+    await transfer.save();
+}
