c3d50f022a82c4cbd5ff580f3e647546b8512504
Erin Shaben <eshaben@icloud.com>
2022-06-22 11:41:38 -0400

    include issued & burned events

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,8 @@
-import { Transfer } from "../types";
+import { Transfer, AssetStatus } from "../types";
 import { SubstrateEvent } from '@subql/types';
 import { Balance } from "@polkadot/types/interfaces";
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
+export async function handleTransferredEvent(event: SubstrateEvent): Promise<void> {
     const assetId = event.event.data[0];
     const from = event.event.data[1];
     const to = event.event.data[2];
@@ -12,8 +12,41 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
 
     transfer.assetId = assetId.toString();
     transfer.from = from.toString();
-    transfer.to = to.toString()
+    transfer.to = to.toString();
     transfer.balance = (balance as Balance).toBigInt();
+    transfer.status = AssetStatus.TRANSFERRED;
+
+    await transfer.save();
+}
+
+export async function handleIssuedEvent(event: SubstrateEvent): Promise<void> {
+    const assetId = event.event.data[0];
+    const to = event.event.data[1];
+    const balance = event.event.data[2];
+
+    const transfer = new Transfer(`${event.block.block.header.number.toNumber()}-${event.idx}`);
+
+    transfer.assetId = assetId.toString();
+    transfer.from = null;
+    transfer.to = to.toString();
+    transfer.balance = (balance as Balance).toBigInt();
+    transfer.status = AssetStatus.ISSUED;
+
+    await transfer.save();
+}
+
+export async function handleBurnedEvent(event: SubstrateEvent): Promise<void> {
+    const assetId = event.event.data[0];
+    const from = event.event.data[1];
+    const balance = event.event.data[2];
+
+    const transfer = new Transfer(`${event.block.block.header.number.toNumber()}-${event.idx}`);
+
+    transfer.assetId = assetId.toString();
+    transfer.from = from.toString();
+    transfer.to = null;
+    transfer.balance = (balance as Balance).toBigInt();
+    transfer.status = AssetStatus.BURNED;
 
     await transfer.save();
 }
